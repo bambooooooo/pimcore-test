e19585aa84cee8358dc68a90e6ea04d354aa3e9c
Wiktor.Ciupa <it@megstyl.pl>
2025-04-08 13:43:32 +0200

    Move image zip export to download menu

--- public/static/js/startup.js
+++ public/static/js/startup.js
@@ -1,13 +1,21 @@
 document.addEventListener(pimcore.events.postOpenObject, function(e){
+
     if(e.detail.object.data.general.className === "Product" || e.detail.object.data.general.className === "ProductSet")
     {
         e.detail.object.toolbar.add({
-            icon: '/bundles/pimcoreadmin/img/flat-white-icons/gallery.svg',
+            icon: '/bundles/pimcoreadmin/img/flat-white-icons/download-cloud.svg',
             scale: 'medium',
-            handler: function () {
-                const path = "/export/images/" + e.detail.object.id;
-                window.open(path);
-            }
+            menu: [
+                {
+                    text: t('Images'),
+                    icon: '/bundles/pimcoreadmin/img/flat-white-icons/gallery.svg',
+                    scale: 'medium',
+                    handler: function () {
+                        const path = "/export/images/" + e.detail.object.id;
+                        window.open(path);
+                    }
+                }
+            ]
         })
     }
 })
