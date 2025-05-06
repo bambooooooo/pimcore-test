aa3bbdca0b3889061d8c0c7f7d84ec96ecd41c4e
Wiktor.Ciupa <it@megstyl.pl>
2025-05-06 12:24:29 +0200

    Add Gotenberg to PDF printing

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle::class => ['all' => true],
     Pimcore\Bundle\CustomReportsBundle\PimcoreCustomReportsBundle::class => ['all' => true],
+    Pimcore\Bundle\WebToPrintBundle\PimcoreWebToPrintBundle::class => ['all' => true],
 ];
