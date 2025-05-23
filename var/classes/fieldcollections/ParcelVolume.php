ce7947a200a3fe4e1204a7ac876c6ba748ab2da6
Wiktor.Ciupa <it@megstyl.pl>
2025-05-23 12:36:31 +0200

    Pricing Parcel Volume + Raw pricelist print from admin panel

---
+++ var/classes/fieldcollections/ParcelVolume.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * Fields Summary:
+ * - Price [quantityValue]
+ */
+
+return \Pimcore\Model\DataObject\Fieldcollection\Definition::__set_state(array(
+   'dao' => NULL,
+   'key' => 'ParcelVolume',
+   'parentClass' => '',
+   'implementsInterfaces' => '',
+   'title' => '+ Cena za 1m3 paczek',
+   'group' => 'Pricing',
+   'layoutDefinitions' => 
+  \Pimcore\Model\DataObject\ClassDefinition\Layout\Panel::__set_state(array(
+     'name' => NULL,
+     'type' => NULL,
+     'region' => NULL,
+     'title' => NULL,
+     'width' => 0,
+     'height' => 0,
+     'collapsible' => false,
+     'collapsed' => false,
+     'bodyStyle' => NULL,
+     'datatype' => 'layout',
+     'children' => 
+    array (
+      0 => 
+      \Pimcore\Model\DataObject\ClassDefinition\Layout\Panel::__set_state(array(
+         'name' => 'Layout',
+         'type' => NULL,
+         'region' => NULL,
+         'title' => '',
+         'width' => '',
+         'height' => '',
+         'collapsible' => false,
+         'collapsed' => false,
+         'bodyStyle' => '',
+         'datatype' => 'layout',
+         'children' => 
+        array (
+          0 => 
+          \Pimcore\Model\DataObject\ClassDefinition\Data\QuantityValue::__set_state(array(
+             'name' => 'Price',
+             'title' => 'Price',
+             'tooltip' => '',
+             'mandatory' => true,
+             'noteditable' => false,
+             'index' => false,
+             'locked' => false,
+             'style' => '',
+             'permissions' => NULL,
+             'fieldtype' => '',
+             'relationType' => false,
+             'invisible' => false,
+             'visibleGridView' => false,
+             'visibleSearch' => false,
+             'blockedVarsForExport' => 
+            array (
+            ),
+             'unitWidth' => '',
+             'defaultUnit' => 'PLN',
+             'validUnits' => 
+            array (
+              0 => 'PLN',
+            ),
+             'unique' => false,
+             'autoConvert' => false,
+             'defaultValueGenerator' => '',
+             'width' => '',
+             'defaultValue' => NULL,
+             'integer' => false,
+             'unsigned' => false,
+             'minValue' => 0.0,
+             'maxValue' => NULL,
+             'decimalSize' => NULL,
+             'decimalPrecision' => NULL,
+          )),
+        ),
+         'locked' => false,
+         'blockedVarsForExport' => 
+        array (
+        ),
+         'fieldtype' => 'panel',
+         'layout' => NULL,
+         'border' => false,
+         'icon' => '',
+         'labelWidth' => 100,
+         'labelAlign' => 'left',
+      )),
+    ),
+     'locked' => false,
+     'blockedVarsForExport' => 
+    array (
+    ),
+     'fieldtype' => 'panel',
+     'layout' => NULL,
+     'border' => false,
+     'icon' => NULL,
+     'labelWidth' => 100,
+     'labelAlign' => 'left',
+  )),
+   'fieldDefinitionsCache' => NULL,
+   'blockedVarsForExport' => 
+  array (
+  ),
+   'activeDispatchingEvents' => 
+  array (
+  ),
+));
