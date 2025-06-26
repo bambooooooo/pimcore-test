35c53044c20cdd0395c64f6d8ec92ece29855f03
Wiktor.Ciupa <it@megstyl.pl>
2025-06-26 12:10:39 +0200

    Datasheet bool printing and translations
    + Add valid bool printing (text instead of int)
    + Add translation respect for all parameters
    + Add Twig keyConfig extension

--- src/Twig/KeyNameExtension.php
+++ src/Twig/KeyNameExtension.php
@@ -3,6 +3,7 @@
 namespace App\Twig;
 
 use Pimcore\Model\DataObject\Classificationstore;
+use Pimcore\Model\DataObject\Classificationstore\KeyConfig;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -12,6 +13,7 @@ class KeyNameExtension extends AbstractExtension
     {
         return [
             new TwigFunction('keyname', [$this, 'keyname'], ['is_safe' => ['html']]),
+            new TwigFunction('keyConfig', [$this, 'keyConfig'], ['is_safe' => ['html']]),
         ];
     }
 
@@ -24,5 +26,17 @@ class KeyNameExtension extends AbstractExtension
         }
 
         return $k->getTitle();
+    }
+
+    public function keyConfig(int $keyId): KeyConfig
+    {
+        $k = Classificationstore\KeyConfig::getById($keyId);
+
+        if(!$k)
+        {
+            throw new \Exception('Key configuration not found');
+        }
+
+        return $k;
     }
 }
