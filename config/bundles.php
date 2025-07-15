f0f8ce12b3c279c4f1df580674d1ff76df3d8723
Wiktor.Ciupa <it@megstyl.pl>
2025-07-15 12:05:55 +0200

    Add PimcoreSimpleBackendSearchBundle

--- config/bundles.php
+++ config/bundles.php
@@ -4,6 +4,7 @@ return [
     Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle::class => ['all' => true],
     Pimcore\Bundle\CustomReportsBundle\PimcoreCustomReportsBundle::class => ['all' => true],
     Pimcore\Bundle\WebToPrintBundle\PimcoreWebToPrintBundle::class => ['all' => true],
+    Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
     Pimcore\Bundle\DataImporterBundle\PimcoreDataImporterBundle::class => ['all' => true],
     Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
 ];
