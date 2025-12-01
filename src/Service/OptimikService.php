763a14754d46ab8bdd7599ae0730e00fcb2c6400
Wiktor.Ciupa <it@megstyl.pl>
2025-12-01 08:25:14 +0100

    Add 24h cache for Optimik sheets

--- src/Service/OptimikService.php
+++ src/Service/OptimikService.php
@@ -3,11 +3,13 @@
 namespace App\Service;
 
 use Symfony\Component\HttpClient\HttpOptions;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class OptimikService
 {
-    public function __construct(private HttpClientInterface $httpClient, private readonly string $url)
+    public function __construct(private HttpClientInterface $httpClient, private readonly string $url, private readonly CacheInterface $cache)
     {
         $this->httpClient = $this->httpClient->withOptions((new HttpOptions())
             ->setBaseUri($this->url)
@@ -23,7 +25,10 @@ class OptimikService
             $url .= '&searchProduct=' . $product;
         }
 
-        return $this->httpClient->request('GET', $url)->toArray();
+        return $this->cache->get('optimik_' . md5($url), function (ItemInterface $item) use ($url){
+            $item->expiresAfter(60 * 60 * 24);
+            return $this->httpClient->request('GET', $url)->toArray();
+        });
     }
 
     public function getUsedSheets(int $l, int $w, string $m)
