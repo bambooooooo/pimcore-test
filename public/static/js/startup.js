674f2aa4d1553bada39b4bcad800e785405f9a07
Wiktor.Ciupa <it@megstyl.pl>
2026-01-26 10:35:37 +0100

    Add basic brace expansion to objects only
    + can expand only items in given sets
    + E.g.: {A,B}-{1,2,3} -> A1, A2, A3, B1, B2, B3
    + numeric ranges are not supported
    + replace AddSibling function as more general one

--- public/static/js/startup.js
+++ public/static/js/startup.js
@@ -9,6 +9,166 @@ Date.prototype.ddmmyyyy = function() {
     ].join('.');
 }
 
+function bracePair(tkns, iPosn, iNest, lstCommas) {
+    if (iPosn >= tkns.length || iPosn < 0) return null;
+
+    var t = tkns[iPosn],
+        n = (t === '{') ? (
+            iNest + 1
+        ) : (t === '}' ? (
+            iNest - 1
+        ) : iNest),
+        lst = (t === ',' && iNest === 1) ? (
+            lstCommas.concat(iPosn)
+        ) : lstCommas;
+
+    return n ? bracePair(tkns, iPosn + 1, n, lst) : {
+        close: iPosn,
+        commas: lst
+    };
+}
+
+// Parse of a SYNTAGM subtree
+function andTree(dctSofar, tkns) {
+    if (!tkns.length) return [dctSofar, []];
+
+    var dctParse = dctSofar ? dctSofar : {
+            fn: and,
+            args: []
+        },
+
+        head = tkns[0],
+        tail = head ? tkns.slice(1) : [],
+
+        dctBrace = head === '{' ? bracePair(
+            tkns, 0, 0, []
+        ) : null,
+
+        lstOR = dctBrace && (
+            dctBrace.close
+        ) && dctBrace.commas.length ? (
+            splitAt(dctBrace.close + 1, tkns)
+        ) : null;
+
+    return andTree({
+        fn: and,
+        args: dctParse.args.concat(
+            lstOR ? (
+                orTree(dctParse, lstOR[0], dctBrace.commas)
+            ) : head
+        )
+    }, lstOR ? (
+        lstOR[1]
+    ) : tail);
+}
+
+// Parse of a PARADIGM subtree
+function orTree(dctSofar, tkns, lstCommas) {
+    if (!tkns.length) return [dctSofar, []];
+    var iLast = lstCommas.length;
+
+    return {
+        fn: or,
+        args: splitsAt(
+            lstCommas, tkns
+        ).map(function (x, i) {
+            var ts = x.slice(
+                1, i === iLast ? (
+                    -1
+                ) : void 0
+            );
+
+            return ts.length ? ts : [''];
+        }).map(function (ts) {
+            return ts.length > 1 ? (
+                andTree(null, ts)[0]
+            ) : ts[0];
+        })
+    };
+}
+
+// List of unescaped braces and commas, and remaining strings
+function tokens(str) {
+    // Filter function excludes empty splitting artefacts
+    var toS = function (x) {
+        return x.toString();
+    };
+
+    return str.split(/(\\\\)/).filter(toS).reduce(function (a, s) {
+        return a.concat(s.charAt(0) === '\\' ? s : s.split(
+            /(\\*[{,}])/
+        ).filter(toS));
+    }, []);
+}
+
+// PARSE TREE OPERATOR (1 of 2)
+// Each possible head * each possible tail
+function and(args) {
+    var lng = args.length,
+        head = lng ? args[0] : null,
+        lstHead = "string" === typeof head ? (
+            [head]
+        ) : head;
+
+    return lng ? (
+        1 < lng ? lstHead.reduce(function (a, h) {
+            return a.concat(
+                and(args.slice(1)).map(function (t) {
+                    return h + t;
+                })
+            );
+        }, []) : lstHead
+    ) : [];
+}
+
+// PARSE TREE OPERATOR (2 of 2)
+// Each option flattened
+function or(args) {
+    return args.reduce(function (a, b) {
+        return a.concat(b);
+    }, []);
+}
+
+// One list split into two (first sublist length n)
+function splitAt(n, lst) {
+    return n < lst.length + 1 ? [
+        lst.slice(0, n), lst.slice(n)
+    ] : [lst, []];
+}
+
+// One list split into several (sublist lengths [n])
+function splitsAt(lstN, lst) {
+    return lstN.reduceRight(function (a, x) {
+        return splitAt(x, a[0]).concat(a.slice(1));
+    }, [lst]);
+}
+
+// Value of the parse tree
+function evaluated(e) {
+    return typeof e === 'string' ? e :
+        e.fn(e.args.map(evaluated));
+}
+
+// JSON prettyprint (for parse tree, token list etc)
+function pp(e) {
+    return JSON.stringify(e, function (k, v) {
+        return typeof v === 'function' ? (
+            '[function ' + v.name + ']'
+        ) : v;
+    }, 2)
+}
+
+
+// ----------------------- MAIN ------------------------
+// s -> [s]
+function expansions(s) {
+    // BRACE EXPRESSION PARSED
+    var dctParse = andTree(null, tokens(s))[0];
+
+    // AST EVALUATED TO LIST OF STRINGS
+    return evaluated(dctParse);
+}
+
 document.addEventListener(pimcore.events.postOpenAsset, function(e) {
 
     if(e.detail.asset.data.mimetype === 'application/pdf')
@@ -686,7 +846,7 @@ document.addEventListener(pimcore.events.postOpenObject, function(e){
 })
 
 document.addEventListener(pimcore.events.pimcoreReady, (e) => {
-    const ADD_SIBLING_KEY = 'm';
+    const ADD_CHILDREN_KEY = 'm';
 
     const openNewObject = function(){
 
@@ -734,10 +894,101 @@ document.addEventListener(pimcore.events.pimcoreReady, (e) => {
         })
     }
 
+    const addChildren = function(){
+
+        let tabPanel = Ext.getCmp("pimcore_panel_tabs");
+        let activeTab = tabPanel.getActiveTab();
+
+        if(!activeTab)
+        {
+            Ext.MessageBox.show({
+                title: t("Error"),
+                msg: t("Open object first that need to have sibling first."),
+                buttons: Ext.Msg.OK,
+            });
+
+            return;
+        }
+
+        if(activeTab.object == null)
+        {
+            Ext.MessageBox.show({
+                title: t("Error"),
+                msg: t("Can not add children with kind other than object"),
+                buttons: Ext.Msg.OK
+            });
+
+            return;
+        }
+
+        if(activeTab.object.data.type == 'folder')
+        {
+            Ext.MessageBox.show({
+                title: t("Error"),
+                msg: t("Can not add folder children. Objects only."),
+                buttons: Ext.Msg.OK,
+            });
+
+            return;
+        }
+
+        let parentId = activeTab.object.data.general.id;
+
+        var options = {
+            url: Routing.generate('pimcore_admin_dataobject_dataobject_add'),
+            elementType: "object",
+            sourceTree: null,
+            parentId: parentId,
+            className: activeTab.object.data.general.className,
+            classId: activeTab.object.data.general.classId,
+        };
+
+        var ret = Ext.MessageBox.prompt(t("Enter new object(s) key or pattern"), t("Enter new object(s) key or pattern"), function(btn, text){
+            if(btn === 'ok'){
+                var be = expansions(text);
+
+                var total = be.length;
+                var completed = 0;
+                var added = 0;
+                var failed = false;
+
+                function done(res)
+                {
+                    if(JSON.parse(res.responseText).success === true)
+                    {
+                        added++;
+                    }
+
+                    completed++;
+                    if(completed == total)
+                    {
+                        pimcore.helpers.showNotification("Ok", "Dodano: " + added + " / " + completed + " obiektów", "ok", "Ok");
+                        pimcore.treenodelocator.showInTree(parentId, "object");
+                        pimcore.elementservice.refreshNodeAllTrees('object', parentId);
+                    }
+                }
+
+                for(var i=0; i<total; i++)
+                {
+                    options['key'] = pimcore.helpers.getValidFilename(be[i], "object");
+
+                    Ext.Ajax.request({
+                        url: options.url,
+                        method: 'POST',
+                        params: options,
+                        success: function(res){
+                            done(res);
+                        }
+                    });
+                }
+            }
+        })
+    }
+
     new Ext.util.KeyMap({
         target: document,
-        key: ADD_SIBLING_KEY,
-        fn: openNewObject,
+        key: ADD_CHILDREN_KEY,
+        fn: addChildren,
         ctrl: true,
         alt: false,
         shift: false,
