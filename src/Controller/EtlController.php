3723b6eb810a296e14b63b6500b543009e251c1a
Wiktor.Ciupa <it@megstyl.pl>
2026-07-02 14:05:04 +0200

    Add /etl/field/{id}/{expr} endpoint to get any object data (UNSAFE)

--- src/Controller/EtlController.php
+++ src/Controller/EtlController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Pimcore\Controller\FrontendController;
 use Pimcore\Model\DataObject;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Attribute\Route;
@@ -49,5 +51,55 @@ class EtlController extends FrontendController
         $response->headers->set('Content-Disposition', 'attachment; filename="packages.csv"');
 
         return $response;
+    }
+
+    #[Route('/field/{id}/{expression}', name: '_somefield')]
+    public function getSomeField(Request $request): Response
+    {
+        $id = (int)$request->get('id');
+        $expression = $request->get('expression');
+
+        $obj = DataObject::getById($id);
+        if(!$obj)
+        {
+            return new Response('Not found', 404);
+        }
+
+        try
+        {
+            if (preg_match('/^([a-zA-Z0-9_]+)\((.*)\)$/', $expression, $matches)) {
+                $method = $matches[1];
+                $argsRaw = trim($matches[2]);
+
+                $args = [];
+
+                if ($argsRaw !== '') {
+                    $args = array_map(
+                        static fn($arg) => trim($arg, " \t\n\r\0\x0B\"'"),
+                        explode(',', $argsRaw)
+                    );
+                }
+
+                if (!method_exists($obj, $method)) {
+                    throw new \Exception("Method {$method} does not exist");
+                }
+
+                $data = $obj->$method(...$args);
+            } else {
+                $getter = 'get' . ucfirst($expression);
+
+                if (!method_exists($obj, $getter)) {
+                    throw new \Exception("Getter {$getter} does not exist");
+                }
+
+                $data = $obj->$getter();
+            }
+
+            return new Response($data ?? $expression, Response::HTTP_OK);
+        }
+        catch (\Exception $e)
+        {
+            return new Response($e->getMessage(), Response::HTTP_OK);
+        }
     }
 }
